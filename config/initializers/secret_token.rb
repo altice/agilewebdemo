632244de8b65a2660550fb0100e835eddbe4a6ff
MakerSquare Student <students@themakersquare.com>
2014-01-15 12:32:32 -0600

    creating controller with route variables

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '2d83bca8e283806b4caecdb18a1ae016fd934cc9331ab80b6d4777d66f0ceb67fc4c557be54f96c3a9b66487ed0e98ee9fe0733c99bf334fa3a21ce8cdef7910'
